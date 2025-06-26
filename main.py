8f7a088377098a7951e23797fb3f60c2dbe837f6
Nandan Pansuriya <pansuriyanandan01@gmail.com>
2025-06-26 12:17:23 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import webbrowser
 import os
 import keyboard
 import pyautogui
+import datetime
 
 engine = pyttsx3.init()
 
@@ -16,10 +17,11 @@ def take_command():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
+        r.pause_threshold = 1.0
         audio = r.listen(source)
 
         try:
-            command = r.recognize_google(audio)
+            command = r.recognize_google(audio,language="en-in")
             print(f"You said : {command}")
             return command
         except Exception as e:
@@ -27,75 +29,125 @@ def take_command():
             print("Say again ....")
             return "Error"
 
+def greet_user(hour) :
+        
+    if hour < 12 :
+        speak("Good morning, Sir ")
+    elif hour <18 :
+        speak("Good afternoon, Sir")
+    else:
+        speak("Good evening, Sir")
 
 
-speak("Hello, I am jarvis AI...")
+hour = datetime.datetime.now().hour
+
+# speak("Hello, my name is Jarvis. I am your personal assistant. How can I help you today?")
+greet_user(hour)
+
+speak("I am jarvis AI, How can i help you ...")
 
 while True:
     
-    user = take_command()
-    if "hello" in user.lower():
+    user = take_command().lower()
+
+    if user == "error" :
+        continue
+
+    if "hello" in user:
         speak("Hello sir, How can i help you ...")
 
-    if "open youtube" in user.lower():
+    if "open youtube" in user :
+        speak("opening Youtube ...")
         # webbrowser.open("https://youtube.com")
         os.startfile("C:/Users/pansu/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Chrome Apps/YouTube.lnk")
-        speak("opening Youtube ...")
     
-    elif "open github" in user.lower():
+    elif "open github" in user :
+        speak("opening Github ...")
         # webbrowser.open("https;//github.com")
         os.startfile("C:/Users/pansu/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Chrome Apps/github.lnk")
-        speak("opening Github ...")
 
-    elif "open classroom" in user.lower():
+    elif "open classroom" in user :
+        speak("opening Classroom ...")
         # webbrowser.open("https://classroom.google.com")
         os.startfile("C:/Users/pansu/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Chrome Apps/Google Classroom.lnk")
-        speak("opening Classroom ...")
 
-    elif "open vs code" in user.lower():
-        os.startfile("C:/Users/pansu/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Visual Studio Code/Visual Studio Code.lnk")
+    elif "open vs code" in user :
         speak("opening VS code ...")
+        os.startfile("C:/Users/pansu/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Visual Studio Code/Visual Studio Code.lnk")
 
-    elif "open chrome" in user.lower():
-        os.system("start chrome")        
+    elif "open chrome" in user :
         speak("opening Chrome ...")
+        os.system("start chrome")        
 
-    elif "open google" in user.lower():
-        webbrowser.open("https://google.com")
+    elif "open google" in user :
         speak("opening Google ...")
+        webbrowser.open("https://google.com")
 
-    elif "open linkedin" in user.lower():
+    elif "open linkedin" in user :
+        speak("opening Linkedin ...")
         # webbrowser.open("https://linkedin.com")
         os.system("start linkedin:")
-        speak("opening Linkedin ...")
 
-    elif "open whatsapp" in user.lower():
+    elif "open whatsapp" in user :
+        speak("opening WhatsApp ...")
         # webbrowser.open("https://whatsapp.com")
         os.system("start whatsapp:")
-        speak("opening WhatsApp ...")
 
-    elif "open notepad" in user.lower():
-        os.system("start notepad")
+    elif "open notepad" in user :
         speak("opening Notepad ...")
+        os.system("start notepad")
 
-    elif "open chatgpt" in user.lower():
-        webbrowser.open("https://chatgpt.com")
+    elif "open chatgpt" in user :
         speak("opening ChatGPT ...")
+        webbrowser.open("https://chatgpt.com")
 
-    elif "open file explorer" in user.lower():
-        os.system("start explorer")
+    elif "open file explorer" in user :
         speak("opening File Explorer ...")
+        os.system("start explorer")
 
-    elif "open command prompt"  in user.lower():
-        os.system("start cmd")
+    elif "open command prompt"  in user :
         speak("opening Command Prompt ...")  
+        os.system("start cmd")
 
-    
-    
-    
+    elif "open spotify" in user :
+        speak("opening Spotify ...")
+        os.system("start spotify")
 
-    if "exit" in user.lower():
+    elif "open start menu" in user :
+        pyautogui.press('win')
+
+
+
+
+    # if "minimize window" in user :
+    #     pyautogui.hotkey('win','down')
+    
+    # elif "maximize window" in user :
+    #     pyautogui.hotkey('win','up')
+
+
+    if "switch window" in user :
+        pyautogui.hotkey('alt','tab')  
+
+    elif "enter" in user :
+        keyboard.press('enter') 
+    
+    elif "Focus on search bar" in user:
+        pyautogui.hotkey('ctrl','l')  
+
+    elif "new tab" in user :
+        pyautogui.hotkey('ctrl','n')   
+
+    elif "close tab" or "close current tab" in user :
+        pyautogui.hotkey('ctrl','w')
+    
+    elif "close all tab" in user :
+        pyautogui.hotkey('ctrl','shift','w')
+    
+    elif "close window" or "close current window" in user :
+        keyboard.send("alt+tab")
+
+
+    if "exit" in user :
         speak("Shutting down.")
         exit()
-
-    
